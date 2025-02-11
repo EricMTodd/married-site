e215fc81dce3846ed91a39c4262f6878f4d26762
EricMTodd <eterictodd@gmail.com>
2025-02-11 11:51:08 -0700

    Added all pictures to gallery.

--- app.py
+++ app.py
@@ -10,7 +10,19 @@ def home():
 @app.route('/gallery')
 def gallery():
     engagement_pictures = os.listdir(os.path.join(app.static_folder, "images/Engagement"))
-    return render_template('partials/gallery.html', engagement_pictures=engagement_pictures)
+    getting_ready_pictures = os.listdir(os.path.join(app.static_folder, "images/Getting ready"))
+    mr_and_mrs_pictures = os.listdir(os.path.join(app.static_folder, "images/Mr & Mrs"))
+    portraits_pictures = os.listdir(os.path.join(app.static_folder, "images/Portraits"))
+    reception_pictures = os.listdir(os.path.join(app.static_folder, "images/Reception"))
+    rings_pictures = os.listdir(os.path.join(app.static_folder, "images/Rings"))
+    return render_template('partials/gallery.html', 
+                           engagement_pictures=engagement_pictures,
+                           getting_ready_pictures=getting_ready_pictures,
+                           mr_and_mrs_pictures=mr_and_mrs_pictures,
+                           portraits_pictures=portraits_pictures,
+                           reception_pictures=reception_pictures,
+                           rings_pictures=rings_pictures
+                           )
 
 @app.route('/guest-list')
 def guest_list():
